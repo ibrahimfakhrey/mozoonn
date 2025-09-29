38c8e529f6bdbbfe36e49f05d96874bb16696070
Ibrahim fakhrey <77210469+ibrahimfakhrey@users.noreply.github.com>
2025-09-29 13:04:20 +0300

    Extract inline assets into separate files

---
+++ scripts.js
@@ -0,0 +1,297 @@
+{"@context":"https:\/\/schema.org","@graph":[{"@type":"BreadcrumbList","@id":"https:\/\/www.alfareedagroupintl.com\/ar\/#breadcrumblist","itemListElement":[{"@type":"ListItem","@id":"https:\/\/www.alfareedagroupintl.com\/ar\/#listItem","position":1,"name":"Home"}]},{"@type":"Organization","@id":"https:\/\/www.alfareedagroupintl.com\/ar\/#organization","name":"AL  Najma Fareeda International Group","url":"https:\/\/www.alfareedagroupintl.com\/ar\/"},{"@type":"WebPage","@id":"https:\/\/www.alfareedagroupintl.com\/ar\/#webpage","url":"https:\/\/www.alfareedagroupintl.com\/ar\/","name":"Home - AL Najma Fareeda International Group","inLanguage":"en-US","isPartOf":{"@id":"https:\/\/www.alfareedagroupintl.com\/ar\/#website"},"breadcrumb":{"@id":"https:\/\/www.alfareedagroupintl.com\/ar\/#breadcrumblist"},"datePublished":"2022-11-16T11:47:28+00:00","dateModified":"2023-09-26T03:34:30+00:00"},{"@type":"WebSite","@id":"https:\/\/www.alfareedagroupintl.com\/ar\/#website","url":"https:\/\/www.alfareedagroupintl.com\/ar\/","name":"AL  Najma Fareeda International Group","inLanguage":"en-US","publisher":{"@id":"https:\/\/www.alfareedagroupintl.com\/ar\/#organization"},"potentialAction":{"@type":"SearchAction","target":{"@type":"EntryPoint","urlTemplate":"https:\/\/www.alfareedagroupintl.com\/ar\/?s={search_term_string}"},"query-input":"required name=search_term_string"}}]}
+
+/* <![CDATA[ */
+window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/16.0.1\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/16.0.1\/svg\/","svgExt":".svg","source":{"concatemoji":"https:\/\/www.alfareedagroupintl.com\/ar\/wp-includes\/js\/wp-emoji-release.min.js?ver=6.8.2"}};
+/*! This file is auto-generated */
+!function(s,n){var o,i,e;function c(e){try{var t={supportTests:e,timestamp:(new Date).valueOf()};sessionStorage.setItem(o,JSON.stringify(t))}catch(e){}}function p(e,t,n){e.clearRect(0,0,e.canvas.width,e.canvas.height),e.fillText(t,0,0);var t=new Uint32Array(e.getImageData(0,0,e.canvas.width,e.canvas.height).data),a=(e.clearRect(0,0,e.canvas.width,e.canvas.height),e.fillText(n,0,0),new Uint32Array(e.getImageData(0,0,e.canvas.width,e.canvas.height).data));return t.every(function(e,t){return e===a[t]})}function u(e,t){e.clearRect(0,0,e.canvas.width,e.canvas.height),e.fillText(t,0,0);for(var n=e.getImageData(16,16,1,1),a=0;a<n.data.length;a++)if(0!==n.data[a])return!1;return!0}function f(e,t,n,a){switch(t){case"flag":return n(e,"\ud83c\udff3\ufe0f\u200d\u26a7\ufe0f","\ud83c\udff3\ufe0f\u200b\u26a7\ufe0f")?!1:!n(e,"\ud83c\udde8\ud83c\uddf6","\ud83c\udde8\u200b\ud83c\uddf6")&&!n(e,"\ud83c\udff4\udb40\udc67\udb40\udc62\udb40\udc65\udb40\udc6e\udb40\udc67\udb40\udc7f","\ud83c\udff4\u200b\udb40\udc67\u200b\udb40\udc62\u200b\udb40\udc65\u200b\udb40\udc6e\u200b\udb40\udc67\u200b\udb40\udc7f");case"emoji":return!a(e,"\ud83e\udedf")}return!1}function g(e,t,n,a){var r="undefined"!=typeof WorkerGlobalScope&&self instanceof WorkerGlobalScope?new OffscreenCanvas(300,150):s.createElement("canvas"),o=r.getContext("2d",{willReadFrequently:!0}),i=(o.textBaseline="top",o.font="600 32px Arial",{});return e.forEach(function(e){i[e]=t(o,e,n,a)}),i}function t(e){var t=s.createElement("script");t.src=e,t.defer=!0,s.head.appendChild(t)}"undefined"!=typeof Promise&&(o="wpEmojiSettingsSupports",i=["flag","emoji"],n.supports={everything:!0,everythingExceptFlag:!0},e=new Promise(function(e){s.addEventListener("DOMContentLoaded",e,{once:!0})}),new Promise(function(t){var n=function(){try{var e=JSON.parse(sessionStorage.getItem(o));if("object"==typeof e&&"number"==typeof e.timestamp&&(new Date).valueOf()<e.timestamp+604800&&"object"==typeof e.supportTests)return e.supportTests}catch(e){}return null}();if(!n){if("undefined"!=typeof Worker&&"undefined"!=typeof OffscreenCanvas&&"undefined"!=typeof URL&&URL.createObjectURL&&"undefined"!=typeof Blob)try{var e="postMessage("+g.toString()+"("+[JSON.stringify(i),f.toString(),p.toString(),u.toString()].join(",")+"));",a=new Blob([e],{type:"text/javascript"}),r=new Worker(URL.createObjectURL(a),{name:"wpTestEmojiSupports"});return void(r.onmessage=function(e){c(n=e.data),r.terminate(),t(n)})}catch(e){}c(n=g(i,f,p,u))}t(n)}).then(function(e){for(var t in e)n.supports[t]=e[t],n.supports.everything=n.supports.everything&&n.supports[t],"flag"!==t&&(n.supports.everythingExceptFlag=n.supports.everythingExceptFlag&&n.supports[t]);n.supports.everythingExceptFlag=n.supports.everythingExceptFlag&&!n.supports.flag,n.DOMReady=!1,n.readyCallback=function(){n.DOMReady=!0}}).then(function(){return e}).then(function(){var e;n.supports.everything||(n.readyCallback(),(e=n.source||{}).concatemoji?t(e.concatemoji):e.wpemoji&&e.twemoji&&(t(e.twemoji),t(e.wpemoji)))}))}((window,document),window._wpemojiSettings);
+/* ]]> */
+
+/* <![CDATA[ */
+/* Run FooBox FREE (v2.7.17) */
+var FOOBOX = window.FOOBOX = {
+	ready: true,
+	disableOthers: false,
+	o: {wordpress: { enabled: true }, countMessage:'image %index of %total', excludes:'.fbx-link,.nofoobox,.nolightbox,a[href*="pinterest.com/pin/create/button/"]', affiliate : { enabled: false }},
+	selectors: [
+		".foogallery-container.foogallery-lightbox-foobox", ".foogallery-container.foogallery-lightbox-foobox-free", ".gallery", ".wp-block-gallery", ".wp-caption", ".wp-block-image", "a:has(img[class*=wp-image-])", ".foobox"
+	],
+	pre: function( $ ){
+		// Custom JavaScript (Pre)
+		
+	},
+	post: function( $ ){
+		// Custom JavaScript (Post)
+		
+		// Custom Captions Code
+		
+	},
+	custom: function( $ ){
+		// Custom Extra JS
+		
+	}
+};
+/* ]]> */
+
+//<![CDATA[
+   $(window).load(function() { // makes sure the whole site is loaded
+      $('#status').fadeOut(); // will first fade out the loading animation
+      $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+      $('body').delay(350).css({
+         'overflow': 'visible'
+      });
+   })
+   //]]>
+
+(function($) {
+      $(function() {
+         $(".scroller").simplyScroll();
+      });
+   })(jQuery);
+
+$(document).ready(function() {
+         $('#horizontalTab').easyResponsiveTabs({
+            type: 'default', //Types: default, vertical, accordion           
+            width: 'auto', //auto or any width like 600px
+            fit: true, // 100% fit in a container
+            closed: 'accordion', // Start closed if in accordion view
+            activate: function(event) { // Callback function if tab is switched
+               var $tab = $(this);
+               var $info = $('#tabInfo');
+               var $name = $('span', $info);
+
+               $name.text($tab.text());
+
+               $info.show();
+            }
+         });
+
+         $('#verticalTab').easyResponsiveTabs({
+            type: 'vertical',
+            width: 'auto',
+            height: 'auto',
+            fit: true
+         });
+      });
+
+jQuery(document).ready(function() {
+         // CUSTOM AJAX CONTENT LOADING FUNCTION
+         var ajaxRevslider = function(obj) {
+
+            // obj.type : Post Type
+            // obj.id : ID of Content to Load
+            // obj.aspectratio : The Aspect Ratio of the Container / Media
+            // obj.selector : The Container Selector where the Content of Ajax will be injected. It is done via the Essential Grid on Return of Content
+
+            var content = "";
+
+            data = {};
+
+            data.action = 'revslider_ajax_call_front';
+            data.client_action = 'get_slider_html';
+            data.token = '4424738118';
+            data.type = obj.type;
+            data.id = obj.id;
+            data.aspectratio = obj.aspectratio;
+
+            // SYNC AJAX REQUEST
+            jQuery.ajax({
+               type: "post",
+               url: "",
+               dataType: 'json',
+               data: data,
+               async: false,
+               success: function(ret, textStatus, XMLHttpRequest) {
+                  if (ret.success == true)
+                     content = ret.data;
+               },
+               error: function(e) {
+                  console.log(e);
+               }
+            });
+
+            // FIRST RETURN THE CONTENT WHEN IT IS LOADED !!
+            return content;
+         };
+
+         // CUSTOM AJAX FUNCTION TO REMOVE THE SLIDER
+         var ajaxRemoveRevslider = function(obj) {
+            return jQuery(obj.selector + " .rev_slider").revkill();
+         };
+
+         // EXTEND THE AJAX CONTENT LOADING TYPES WITH TYPE AND FUNCTION
+         var extendessential = setInterval(function() {
+            if (jQuery.fn.tpessential != undefined) {
+               clearInterval(extendessential);
+               if (typeof(jQuery.fn.tpessential.defaults) !== 'undefined') {
+                  jQuery.fn.tpessential.defaults.ajaxTypes.push({
+                     type: "revslider",
+                     func: ajaxRevslider,
+                     killfunc: ajaxRemoveRevslider,
+                     openAnimationSpeed: 0.3
+                  });
+                  // type:  Name of the Post to load via Ajax into the Essential Grid Ajax Container
+                  // func: the Function Name which is Called once the Item with the Post Type has been clicked
+                  // killfunc: function to kill in case the Ajax Window going to be removed (before Remove function !
+                  // openAnimationSpeed: how quick the Ajax Content window should be animated (default is 0.3)
+               }
+            }
+         }, 30);
+      });
+
+var setREVStartSize = function() {
+         var tpopt = new Object();
+         tpopt.startwidth = 1500;
+         tpopt.startheight = 1000;
+         tpopt.container = jQuery('#rev_slider_5_1');
+         tpopt.fullScreen = "on";
+         tpopt.forceFullWidth = "on";
+
+         tpopt.container.closest(".rev_slider_wrapper").css({
+            height: tpopt.container.height()
+         });
+         tpopt.width = parseInt(tpopt.container.width(), 0);
+         tpopt.height = parseInt(tpopt.container.height(), 0);
+         tpopt.bw = tpopt.width / tpopt.startwidth;
+         tpopt.bh = tpopt.height / tpopt.startheight;
+         if (tpopt.bh > tpopt.bw) tpopt.bh = tpopt.bw;
+         if (tpopt.bh < tpopt.bw) tpopt.bw = tpopt.bh;
+         if (tpopt.bw < tpopt.bh) tpopt.bh = tpopt.bw;
+         if (tpopt.bh > 1) {
+            tpopt.bw = 1;
+            tpopt.bh = 1
+         }
+         if (tpopt.bw > 1) {
+            tpopt.bw = 1;
+            tpopt.bh = 1
+         }
+         tpopt.height = Math.round(tpopt.startheight * (tpopt.width / tpopt.startwidth));
+         if (tpopt.height > tpopt.startheight && tpopt.autoHeight != "on") tpopt.height = tpopt.startheight;
+         if (tpopt.fullScreen == "on") {
+            tpopt.height = tpopt.bw * tpopt.startheight;
+            var cow = tpopt.container.parent().width();
+            var coh = jQuery(window).height();
+            if (tpopt.fullScreenOffsetContainer != undefined) {
+               try {
+                  var offcontainers = tpopt.fullScreenOffsetContainer.split(",");
+                  jQuery.each(offcontainers, function(e, t) {
+                     coh = coh - jQuery(t).outerHeight(true);
+                     if (coh < tpopt.minFullScreenHeight) coh = tpopt.minFullScreenHeight
+                  })
+               } catch (e) {}
+            }
+            tpopt.container.parent().height(coh);
+            tpopt.container.height(coh);
+            tpopt.container.closest(".rev_slider_wrapper").height(coh);
+            tpopt.container.closest(".forcefullwidth_wrapper_tp_banner").find(".tp-fullwidth-forcer").height(coh);
+            tpopt.container.css({
+               height: "100%"
+            });
+            tpopt.height = coh;
+         } else {
+            tpopt.container.height(tpopt.height);
+            tpopt.container.closest(".rev_slider_wrapper").height(tpopt.height);
+            tpopt.container.closest(".forcefullwidth_wrapper_tp_banner").find(".tp-fullwidth-forcer").height(tpopt.height);
+         }
+      };
+
+      /* CALL PLACEHOLDER */
+      setREVStartSize();
+
+
+      var tpj = jQuery;
+      tpj.noConflict();
+      var revapi5;
+
+      tpj(document).ready(function() {
+
+         if (tpj('#rev_slider_5_1').revolution == undefined) {
+            revslider_showDoubleJqueryError('#rev_slider_5_1');
+         } else {
+            revapi5 = tpj('#rev_slider_5_1').show().revolution({
+               startDelay: 300,
+               dottedOverlay: "none",
+               delay: 4000,
+               startwidth: 1500,
+               startheight: 1000,
+               hideThumbs: 200,
+
+               thumbWidth: 100,
+               thumbHeight: 50,
+               thumbAmount: 5,
+
+
+               simplifyAll: "on",
+
+               navigationType: "none",
+               navigationArrows: "solo",
+               navigationStyle: "round",
+
+               touchenabled: "on",
+               onHoverStop: "off",
+               nextSlideOnWindowFocus: "off",
+
+               swipe_threshold: 0.7,
+               swipe_min_touches: 1,
+               drag_block_vertical: false,
+
+
+
+               keyboardNavigation: "on",
+
+               navigationHAlign: "center",
+               navigationVAlign: "bottom",
+               navigationHOffset: 0,
+               navigationVOffset: 20,
+
+               soloArrowLeftHalign: "left",
+               soloArrowLeftValign: "center",
+               soloArrowLeftHOffset: 20,
+               soloArrowLeftVOffset: 0,
+
+               soloArrowRightHalign: "right",
+               soloArrowRightValign: "center",
+               soloArrowRightHOffset: 20,
+               soloArrowRightVOffset: 0,
+
+               shadow: 0,
+               fullWidth: "off",
+               fullScreen: "on",
+
+               spinner: "spinner2",
+
+               stopLoop: "off",
+               stopAfterLoops: -1,
+               stopAtSlide: -1,
+
+               shuffle: "off",
+
+
+               forceFullWidth: "on",
+               fullScreenAlignForce: "on",
+               minFullScreenHeight: "",
+               hideTimerBar: "on",
+               hideThumbsOnMobile: "off",
+               hideNavDelayOnMobile: 1500,
+               hideBulletsOnMobile: "off",
+               hideArrowsOnMobile: "off",
+               hideThumbsUnderResolution: 0,
+
+               fullScreenOffsetContainer: "#wpadminbar, #top-nav-wrapper",
+               fullScreenOffset: "",
+               hideSliderAtLimit: 0,
+               hideCaptionAtLimit: 0,
+               hideAllCaptionAtLilmit: 0,
+               startWithSlide: 0
+            });
+
+
+
+
+         }
+      }); /*ready*/
+
+{"prefetch":[{"source":"document","where":{"and":[{"href_matches":"\/ar\/*"},{"not":{"href_matches":["\/ar\/wp-*.php","\/ar\/wp-admin\/*","\/ar\/wp-content\/uploads\/*","\/ar\/wp-content\/*","\/ar\/wp-content\/plugins\/*","\/ar\/wp-content\/themes\/al-najma-fareeda\/*","\/ar\/*\\?(.+)"]}},{"not":{"selector_matches":"a[rel~=\"nofollow\"]"}},{"not":{"selector_matches":".no-prefetch, .no-prefetch a"}}]},"eagerness":"conservative"}]}
+
+/* <![CDATA[ */
+var wpcf7 = {"api":{"root":"https:\/\/www.alfareedagroupintl.com\/ar\/wp-json\/","namespace":"contact-form-7\/v1"}};
+/* ]]> */
